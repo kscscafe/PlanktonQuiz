7a3682239f88c2be7ed254ec7bebe59888fbddf9
kscscafe <sugizaki502@yahoo.co.jp>
2025-01-01 13:37:54 +0900

    Initial Commit

---
+++ PlanktonQuiz/PlanktonQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanktonQuizApp.swift
+//  PlanktonQuiz
+//
+//  Created by 杉崎康隆 on 2025/01/01.
+//
+
+import SwiftUI
+
+@main
+struct PlanktonQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
